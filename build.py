201143acbb8c2e39ed64663f9c454096a5098126
Nicola Ruaro <ruaronicola@gmail.com>
2023-02-20 14:25:19 -0800

    make title lowercase

--- build.py
+++ build.py
@@ -24,7 +24,7 @@ TEMPLATE = """
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title>UNDEREXPOSED</title>
+    <title>underexposed</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/main.css">
